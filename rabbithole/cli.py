c31f831eefe47de8a7015d82c9025fd35d473029
Javier Collado <javier@gigaspaces.com>
2017-01-12 18:42:35 +0100

    Refactor main function
    Some code has been moved to smaller functions easier to maintain and
    test

--- rabbithole/cli.py
+++ rabbithole/cli.py
@@ -7,6 +7,7 @@ import logging
 import os
 import sys
 import threading
+import time
 import traceback
 
 from pprint import pformat
@@ -19,6 +20,10 @@ from rabbithole.sql import Database
 from rabbithole.batcher import Batcher
 
 LOGGER = logging.getLogger(__name__)
+BLOCK_CLASSES = {
+    'amqp': Consumer,
+    'sql': Database,
+}
 
 
 def main(argv=None):
@@ -36,87 +41,122 @@ def main(argv=None):
     configure_logging(args.log_level)
     logging.debug('Configuration:\n%s', pformat(config))
 
-    block_classes = {
-        'amqp': Consumer,
-        'sql': Database,
+    namespace = {
+        block['name']: create_block_instance(block)
+        for block in config['blocks']
     }
-    namespace = {}
-    for block in config['blocks']:
-        block_class = block_classes[block['type']]
-
-        try:
-            block_instance = block_class(
-                *block.get('args', []),
-                **block.get('kwargs', {})
-            )
-        except:
-            LOGGER.error(traceback.format_exc())
-            LOGGER.error(
-                'Unable to create %r block: (type: %r, args: %r, kwargs: %r)',
-                block['name'],
-                block['type'],
-                block.get('args', []),
-                block.get('kwargs', {}),
-            )
-            return 1
-
-        namespace[block['name']] = block_instance
-
+    batcher_config = {
+        'size_limit': config.get('size_limit'),
+        'time_limit': config.get('time_limit'),
+    }
     for flow in config['flows']:
-        input_block, output_block = flow
-        input_block_instance = namespace[input_block['name']]
+        create_flow(flow, namespace, batcher_config)
+    run_input_blocks(namespace)
 
-        try:
-            input_signal = input_block_instance(
-                *input_block.get('args', []),
-                **input_block.get('kwargs', {})
-            )
-        except:
-            LOGGER.error(traceback.format_exc())
-            LOGGER.error(
-                'Unable to get signal from %r block: (args: %r, kwargs: %r)',
-                input_block['name'],
-                input_block.get('args', []),
-                input_block.get('kwargs', {}),
-            )
-            return 1
+    try:
+        # Loop needed to be able to catch KeyboardInterrupt
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        LOGGER.info('Interrupted by user')
 
-        output_block_instance = namespace[output_block['name']]
+    return 0
 
-        try:
-            output_cb = output_block_instance(
-                *output_block.get('args', []),
-                **output_block.get('kwargs', {})
-            )
-        except:
-            LOGGER.error(traceback.format_exc())
-            LOGGER.error(
-                'Unable to get callback from %r block: (args: %r, kwargs: %r)',
-                output_block['name'],
-                output_block.get('args', []),
-                output_block.get('kwargs', {}),
-            )
-            return 1
 
-        batcher = Batcher(config.get('size_limit'), config.get('time_limit'))
-        input_signal.connect(batcher.message_received_cb, weak=False)
-        batcher.batch_ready.connect(output_cb, weak=False)
+def create_block_instance(block):
+    """Create block instance from its configuration
 
+    :param block: Block configuration
+    :type block: dict(str)
+    :return: Block instance
+    :rtype: instance
+
+    """
+    block_class = BLOCK_CLASSES[block['type']]
+
+    try:
+        block_instance = block_class(
+            *block.get('args', []),
+            **block.get('kwargs', {})
+        )
+    except Exception:
+        LOGGER.error(traceback.format_exc())
+        LOGGER.error(
+            'Unable to create %r block: (type: %r, args: %r, kwargs: %r)',
+            block['name'],
+            block['type'],
+            block.get('args', []),
+            block.get('kwargs', {}),
+        )
+        sys.exit(1)
+
+    return block_instance
+
+
+def create_flow(flow, namespace, batcher_config):
+    """Create flow by connecting block signals.
+
+    :param flow: Flow configuration
+    :type flow: dict(str)
+    :param namespace: Block instances namespace
+    :type namespace: dict(str, instance)
+    :param batcher_config: Configuration to be passed to batcher objects
+    :type batcher_config: dict(str)
+
+    """
+    input_block, output_block = flow
+    input_block_instance = namespace[input_block['name']]
+
+    try:
+        input_signal = input_block_instance(
+            *input_block.get('args', []),
+            **input_block.get('kwargs', {})
+        )
+    except Exception:
+        LOGGER.error(traceback.format_exc())
+        LOGGER.error(
+            'Unable to get signal from %r block: (args: %r, kwargs: %r)',
+            input_block['name'],
+            input_block.get('args', []),
+            input_block.get('kwargs', {}),
+        )
+        sys.exit(1)
+
+    output_block_instance = namespace[output_block['name']]
+
+    try:
+        output_cb = output_block_instance(
+            *output_block.get('args', []),
+            **output_block.get('kwargs', {})
+        )
+    except Exception:
+        LOGGER.error(traceback.format_exc())
+        LOGGER.error(
+            'Unable to get callback from %r block: (args: %r, kwargs: %r)',
+            output_block['name'],
+            output_block.get('args', []),
+            output_block.get('kwargs', {}),
+        )
+        sys.exit(1)
+
+    batcher = Batcher(**batcher_config)
+    input_signal.connect(batcher.message_received_cb, weak=False)
+    batcher.batch_ready.connect(output_cb, weak=False)
+
+
+def run_input_blocks(namespace):
+    """Run inputs blocks and start receiving messages from them.
+
+    :param namespace: Block instances namespace
+    :type namespace: dict(str, instance)
+
+    """
     for block_name, block_instance in six.iteritems(namespace):
         run_method = getattr(block_instance, 'run', None)
         if run_method:
             thread = threading.Thread(name=block_name, target=run_method)
             thread.daemon = True
             thread.start()
-
-    try:
-        # Loop needed to be able to catch KeyboardInterrupt
-        while True:
-            thread.join(1)
-    except KeyboardInterrupt:
-        LOGGER.info('Interrupted by user')
-
-    return 0
 
 
 def parse_arguments(argv):
