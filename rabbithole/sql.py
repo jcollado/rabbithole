261a9023822ed7fbf9af9570b64d7031143b03dc
Javier Collado <javier@gigaspaces.com>
2017-05-01 14:10:53 +0200

    Add parameters support to SQL query
    When inserting documents, it's possible to specify a list of parameters
    to pass to the query as positional arguments. If nothing passed, the
    message itself will be used.

--- rabbithole/sql.py
+++ rabbithole/sql.py
@@ -7,11 +7,12 @@ import traceback
 
 from functools import partial
 
-from sqlalchemy import (
-    create_engine,
-    text,
-)
+from sqlalchemy import create_engine
 from sqlalchemy.exc import SQLAlchemyError
+from typing import (  # noqa
+    List,
+    Optional,
+)
 
 LOGGER = logging.getLogger(__name__)
 
@@ -32,30 +33,51 @@ class Database(object):
         self.connection = engine.connect()
         LOGGER.debug('Connected to: %r', url)
 
-    def __call__(self, query):
-        # type: (str) -> partial
+    def __call__(self, query, parameters=None):
+        # type: (str, Optional[List]) -> partial
         """Return callback to use when a batch is ready.
 
         :param query: The query to execute to insert the batch
         :type query: str
+        :param parameters: Parameters to pass to the query on execution
+        :type parameters: list | None
 
         """
-        return partial(self.batch_ready_cb, query=text(query))
+        return partial(
+            self.batch_ready_cb,
+            query=query,
+            parameters=parameters,
+        )
 
-    def batch_ready_cb(self, sender, query, batch):
-        # type: (object, object, List[Dict[str, object]]) -> None
+    def batch_ready_cb(
+            self,
+            sender,  # type: object
+            query,  # type: object
+            parameters,  # type: Optional[List]
+            batch,  # type: List[Dict[str, object]]
+                ):
         """Execute insert query for the batch that is ready.
 
         :param sender: The batcher who sent the batch_ready signal
         :type sender: rabbithole.batcher.Batcher
         :param query: The query to execute to insert the batch
         :type query: :class:`sqlalchemy.sql.elements.TextClause`
-        :param batch: Batch of rows to insert
-        :type rows: list(dict(str))
+        :param batch: Batch of messages
+        :type batch: list(dict(str))
 
         """
+        if parameters is None:
+            batch_parameters = batch
+        else:
+            batch_parameters = self._get_batch_parameters(parameters, batch)
+
         try:
-            self.connection.execute(query, batch)
+            LOGGER.info(
+                'Executing query: (query: %s, parameters: %s)',
+                query,
+                batch_parameters,
+            )
+            self.connection.execute(query, batch_parameters)
         except SQLAlchemyError:
             LOGGER.error(traceback.format_exc())
             LOGGER.error(
@@ -65,3 +87,50 @@ class Database(object):
             )
         else:
             LOGGER.debug('Inserted %d rows', len(batch))
+
+    def _get_batch_parameters(self, parameters, batch):
+        """Get query parameters for a batch of messages.
+
+        :param parameters: Mapping from message to query parameters.
+        :type parameters: list(str)
+        :param batch: Batch of messages
+        :type batch: list(dict(str))
+
+        """
+        batch_parameters = [
+            self._get_message_parameters(parameters, message)
+            for message in batch
+        ]
+        return batch_parameters
+
+    def _get_message_parameters(self, parameters, message):
+        """Get query parameters for a message.
+
+        :param parameters: Mapping from message to query parameters.
+        :type parameters: list(str)
+        :param message: A message
+        :type message: dict(str)
+
+        """
+        message_parameters = [
+            self._get_message_parameter(parameter, message)
+            for parameter in parameters
+        ]
+        return message_parameters
+
+    def _get_message_parameter(self, parameter, message):
+        """Get query parameter for a message.
+
+        :param parameters: Mapping from message to query parameter.
+        :type parameters: str
+        :param message: A message
+        :type message: dict(str)
+
+        """
+        keys = parameter.split('.')
+        value = message
+        for key in keys:
+            value = value.get(key)
+            if value is None:
+                break
+        return value
